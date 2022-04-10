b2232db502e9d4428678a1e8d05f4fea0d790b03
Rajat <rajendrasinghjeena13@gmail.com>
2022-04-11 02:59:05 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
-  return (
+  let [data,setdata]=useState([]);
+  function getData(){
+    fetch(`https://reqres.in/api/users?page=1`)
+       .then((Response)=>Response.json())
+       .then((userdata)=>{
+         console.log(userdata);
+        setdata(userdata.data);
+       })
+       .catch((Error)=>{
+         console.log( Error);
+       })
+  };
+ return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      
 
+      <nav className="navv">
+         <ul className="menu">
+             <li><a className="nav_items" href=" "> LetsGrowMore</a></li>
+            
+             <button className='btn' onClick={getData}>Get Users</button>
+         </ul>
+     </nav>
+
+    <div>
+       {
+           data.map((data)=>{
+                  return(
+                    <div className="cards">
+                    <div className="article">
+                       <img  className="img1"
+                       src={data.avatar} alt="tech"/>
+                      <div className="details">
+                          <h3 className="author">ID: {data.id}</h3>
+                          <h3 className="author">EMAIL: {data.email}</h3>
+                          <h3 className="author">Name: {data.first_name} {data.Last_name}</h3>
+                      </div>
+                    </div>
+                    
+                          </div>
+                  )
+                })}
+                </div>
+                </div>
+    );}
 export default App;
+
